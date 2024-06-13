18fe4ce5a33a18ea262e7c6513e050be7a1940b5
anishbahl-credera <anish.bahl@credera.com>
2024-06-13 13:46:09 -0500

    Update pages.config.js

--- group-project-ui/src/modules/pages/pages.config.js
+++ group-project-ui/src/modules/pages/pages.config.js
@@ -1,13 +1,20 @@
 import HomePageComponent from './home-page/home-page.component';
 import ShopPageComponent from './shop-page/shop-page.component';
-import ProductDetailComponent from './productDetail/productDetail.component';
-
-
+import ProductDetailComponent from './productDetail/FranksRedHot';
+import HuyFongConfig from './productDetail/HuyFong';
+import CholulaConfig from './productDetail/Cholula';
+import TexasPeteConfig from './productDetail/TexasPete';
+import TapatioConfig from './productDetail/Tapatio';
+import TabascoConfig from './productDetail/Tabasco';
+import LHS_Config from './productDetail/LHS';
+import ValentinaConfig from './productDetail/Valentina';
+import CrystalConfig from './productDetail/Crystal';
+import Cart from './cart/Cart';
 // Example component to demonstrate routing with multiple pages
 const TestComp = () => (
   <div>Test</div>
 )
-
+ 
 // Config for root routes to easily add/omit routes
 // NOTE: Including linkText property will include route in navigation menu; omit this property for routes which shouldn't be in navigation
 // NOTE: You may need to update this config with more properties and map to the RootLayout if more complex routes are required
@@ -15,10 +22,20 @@ export const routes = [
   { path: '/test', linkText: 'Test', routeComponent: TestComp, exact: true },
   { path: '/shop', linkText: 'Shop', routeComponent: ShopPageComponent, exact: true },
   { path: '/productDetail', linkText: 'productDetail', routeComponent: ProductDetailComponent, exact: true },
+  { path: '/cart', linkText: 'cart', routeComponent: Cart, exact: true },
+  //path for each sauce
+  { path: '/FranksRedHot', linkText: 'FranksRedHot', routeComponent: ProductDetailComponent, exact: true },
+  { path: '/HuyFong', linkText: 'HuyFong', routeComponent: HuyFongConfig, exact: true },
+  { path: '/Cholula', linkText: 'Cholula', routeComponent: CholulaConfig, exact: true },
+  { path: '/TexasPete', linkText: 'Texas Pete', routeComponent: TexasPeteConfig, exact: true },
+  { path: '/Tapatio', linkText: 'Tapatio', routeComponent: TapatioConfig, exact: true },
+  { path: '/Tabasco', linkText: 'Tabasco', routeComponent: TabascoConfig, exact: true },
+  { path: '/LHS', linkText: 'Louisiana', routeComponent: LHS_Config, exact: true },
+  { path: '/Valentina', linkText: 'Valentina', routeComponent: ValentinaConfig, exact: true },
+  { path: '/Crystal', linkText: 'Valentina', routeComponent: CrystalConfig, exact: true },
   // Currently a fallback route which will match if any routes above are not exactly matched
   // NOTE: Fallback routes should always be last
-  { path: '/', linkText: 'Home', routeComponent: HomePageComponent, exact: false }
-  
+  { path: '/', linkText: 'Home', routeComponent: HomePageComponent, exact: false },
 ];
-
+ 
 export default routes;
