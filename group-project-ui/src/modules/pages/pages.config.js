e1752d7e590705ee9c42caacc36a4b3497500985
anishbahl-credera <anish.bahl@credera.com>
2024-06-13 13:39:47 -0500

    Fixed Backend

--- group-project-ui/src/modules/pages/pages.config.js
+++ group-project-ui/src/modules/pages/pages.config.js
@@ -1,7 +1,7 @@
 import HomePageComponent from './home-page/home-page.component';
 import ShopPageComponent from './shop-page/shop-page.component';
 import ProductDetailComponent from './productDetail/productDetail.component';
-
+import Cart from './cart/cart.component';
 
 // Example component to demonstrate routing with multiple pages
 const TestComp = () => (
@@ -15,9 +15,11 @@ export const routes = [
   { path: '/test', linkText: 'Test', routeComponent: TestComp, exact: true },
   { path: '/shop', linkText: 'Shop', routeComponent: ShopPageComponent, exact: true },
   { path: '/productDetail', linkText: 'productDetail', routeComponent: ProductDetailComponent, exact: true },
+  { path: '/cart', linkText: 'cart', routeComponent: Cart, exact: true },
   // Currently a fallback route which will match if any routes above are not exactly matched
   // NOTE: Fallback routes should always be last
-  { path: '/', linkText: 'Home', routeComponent: HomePageComponent, exact: false },
+  { path: '/', linkText: 'Home', routeComponent: HomePageComponent, exact: false }
+  
 ];
 
 export default routes;
