e6f942743d0258eea49305282edafb17f5197292
nathanturen <nathanturen@yahoo.com>
2024-06-14 08:25:52 -0500

    Revert "Merge branch 'main' of https://github.com/anishbahl-credera/FirstTSProject"
    This reverts commit 30d38a5a0464877fd7ac5a9668109f6f236f79a9, reversing
    changes made to f0982ad0b578a6d84c1582c7e86f201580994e99.

--- group-project-ui/src/modules/pages/layouts/root.layout.jsx
+++ group-project-ui/src/modules/pages/layouts/root.layout.jsx
@@ -5,44 +5,16 @@ import {
   Link
 } from "react-router-dom";
 import rootRoutes from '../pages.config';
-import pepperGIF from '../../../images/pepper.gif';
-import { TiShoppingCart } from "react-icons/ti";
-import './navbar.css';
 
+// Utilize a layout to decouple page layout (navigation menu/header/footer/side menu) from main app component
+// https://reactrouter.com/web/guides/quick-start
 export const RootLayout = () => (
-  <Router>
-    <div className="navbar">
-      <ul>
-        <Link to={'/'}>
-          <img src={ pepperGIF } alt="pepperGIF" />
-        </Link>
-        <li className="links">
-          <Link to={'/shop'}> Shop </Link>
-          <div className="dropdown"> 
-            <button className="dropdownButton"> Sauces </button>
-            <div className="dropdownContent">
-              <Link to={'/FranksRedHot'}> Frank's Red Hot </Link>
-              <Link to={'/HuyFong'}> Huy Fong </Link>
-              <Link to={'/Cholula'}> Cholula </Link>
-              <Link to={'/TexasPete'}> Texas Pete </Link>
-              <Link to={'/Tapatio'}> Tapatio </Link>
-              <Link to={'/Tabasco'}> Tabasco </Link>
-              <Link to={'/LHS'}> Louisiana Hot Sauce </Link>
-              <Link to={'/Valentina'}> Valentina </Link>
-              <Link to={'/Crystal'}> Crystal </Link>
-            </div>
-          </div>
-          <div className="icon">
-            <TiShoppingCart />
-          </div>
-        </li>
-      </ul>
-    </div>
-    <Switch>
-      {rootRoutes.map((route, index) => (
-        <Route exact={route.exact} key={index} path={route.path} component={route.routeComponent} />
-      ))}
-    </Switch>
+    <Router>
+      <Switch>
+        {rootRoutes.map((route, index) => (
+          <Route exact={route.exact} key={index} path={route.path} component={route.routeComponent} />
+        ))}
+      </Switch>
   </Router>
 );
 
